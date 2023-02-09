3b4124dc99e6bb9823a645bbafbd2b4a3c202f5d
loopzhou <loopzhou@tencent.com>
2023-02-09 15:52:26 +0800

    feat(init): vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import path from 'path'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // resolve: {
-  //   alias: {
-  //     "@": path.resolve(__dirname, "./src")
-  //   }
-  // }
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'), // 设置 `@` 指向 `src` 目录
+    },
+  },
+  base: './', // 设置打包路径
+  // 打包配置
+  build: {
+    target: 'modules',
+    outDir: 'dist', //指定输出路径
+    assetsDir: 'assets', // 指定生成静态资源的存放路径
+  },
+  server: {
+    port: 7070, // 设置服务启动端口号
+    open: true, // 设置服务启动时是否自动打开浏览器
+    cors: true, // 允许跨域
+  },
 });
